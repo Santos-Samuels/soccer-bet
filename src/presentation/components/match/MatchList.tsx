d439effa705f564fbe8581f98a2a22e8b52475b3
Santos-Samuels <saamsmith15@gmail.com>
2023-02-12 17:05:07 -0300

    ♻️ improving matches page structure

--- src/presentation/components/match/MatchList.tsx
+++ src/presentation/components/match/MatchList.tsx
@@ -1,51 +1,12 @@
-import { useEffect, useState } from "react";
 import { IMatch } from "@domain/model/match";
-import IToggleStatusMatch from "@domain/usecases/toggleStatusMatch";
-import Select from "../ui/select";
 import MatchItem from "./MatchItem";
-
-type ISortOptions = "todos" | "ativo" | "inativo";
-const sortOptions = ["Todos", "Ativo", "Inativo"];
 
 type Props = {
   matches: IMatch[];
-  getMatches: () => Promise<void>;
-  toggleStatusMatchFactory: IToggleStatusMatch;
+  toggleStatusHandler: (matchId: string) => Promise<void>;
 };
 
-const MetchList: React.FC<Props> = ({
-  matches,
-  getMatches,
-  toggleStatusMatchFactory,
-}) => {
-  const [filteredMatches, setFilteredMatches] = useState<IMatch[]>([]);
-  const [filter, setFilter] = useState<ISortOptions>("todos");
-
-  const toggleStatusHandler = async (matchId: string) => {
-    await toggleStatusMatchFactory.execute(matchId);
-    await getMatches();
-  };
-
-  const filterHandler = (filter: ISortOptions) => {
-    if (filter === "ativo") {
-      const filteredMatches = matches.filter((match) => match.isActive);
-      setFilteredMatches(filteredMatches);
-      return;
-    }
-
-    if (filter === "inativo") {
-      const filteredMatches = matches.filter((match) => !match.isActive);
-      setFilteredMatches(filteredMatches);
-      return;
-    }
-
-    setFilteredMatches([]);
-  };
-
-  useEffect(() => {
-    filterHandler(filter);
-  }, [filter]);
-
+const MetchList: React.FC<Props> = ({ matches, toggleStatusHandler }) => {
   if (matches.length === 0) {
     return (
       <h3 className="text-2xl mt-24 text-center">
@@ -56,41 +17,14 @@ const MetchList: React.FC<Props> = ({
 
   return (
     <>
-      <div className="w-48">
-        <Select
-          label="Filtrar por"
-          options={sortOptions}
-          defaultOption={sortOptions[0]}
-          onChange={(e) =>
-            setFilter(e.target.value.toLocaleLowerCase() as ISortOptions)
-          }
-        />
-      </div>
-
       <div className="flex gap-3 flex-wrap mt-5 justify-center">
-        {filter !== "todos" ? (
-          filteredMatches.length > 0 ? (
-            filteredMatches.map((match) => (
-              <MatchItem
-                match={match}
-                key={match.id}
-                toggleStatus={() => toggleStatusHandler(match.id)}
-              />
-            ))
-          ) : (
-            <h3 className="text-2xl mt-5 text-center">
-              Nenhuma partida encontrada!
-            </h3>
-          )
-        ) : (
-          matches.map((match) => (
-            <MatchItem
-              match={match}
-              key={match.id}
-              toggleStatus={() => toggleStatusHandler(match.id)}
-            />
-          ))
-        )}
+        {matches.map((match) => (
+          <MatchItem
+            match={match}
+            key={match.id}
+            toggleStatus={() => toggleStatusHandler(match.id)}
+          />
+        ))}
       </div>
     </>
   );
--- src/presentation/pages/MatchesPage.tsx
+++ src/presentation/pages/MatchesPage.tsx
@@ -2,9 +2,9 @@ import { useEffect, useState } from "react";
 import { BeatLoader } from "react-spinners";
 import { IMatch } from "@domain/model/match";
 import ICreateMatch from "@domain/usecases/match/createMatch";
-import IListMatches from "@domain/usecases/listMatches";
-import IToggleStatusMatch from "@domain/usecases/toggleStatusMatch";
-import { MatchList, PageContainer } from "../components";
+import IListMatches from "@domain/usecases/match/listMatches";
+import IToggleStatusMatch from "@domain/usecases/match/toggleStatusMatch";
+import { MatchList, PageContainer, Select } from "../components";
 import GameForm from "../components/match/MatchForm";
 
 type Props = {
@@ -13,6 +13,9 @@ type Props = {
   toggleStatusMatchFactory: IToggleStatusMatch;
 };
 
+type ISortOptions = "todos" | "ativo" | "inativo";
+const sortOptions = ["Todos", "Ativo", "Inativo"];
+
 const MatchesPage: React.FC<Props> = ({
   listMatches,
   createMatch,
@@ -20,6 +23,8 @@ const MatchesPage: React.FC<Props> = ({
 }) => {
   const [matches, setMatches] = useState<IMatch[]>([]);
   const [isLoading, setIsLoading] = useState<boolean>(false);
+  const [filter, setFilter] = useState<ISortOptions>("todos");
+  const [filteredMatches, setFilteredMatches] = useState<IMatch[]>([]);
 
   const getMatches = async () => {
     setIsLoading(true);
@@ -32,6 +37,31 @@ const MatchesPage: React.FC<Props> = ({
     setIsLoading(false);
   };
 
+  const filterHandler = (filter: ISortOptions) => {
+    if (filter === "ativo") {
+      const filteredMatches = matches.filter((match) => match.isActive);
+      setFilteredMatches(filteredMatches);
+      return;
+    }
+
+    if (filter === "inativo") {
+      const filteredMatches = matches.filter((match) => !match.isActive);
+      setFilteredMatches(filteredMatches);
+      return;
+    }
+
+    setFilteredMatches([]);
+  };
+
+  const toggleStatusHandler = async (matchId: string) => {
+    await toggleStatusMatchFactory.execute(matchId);
+    await getMatches();
+  };
+
+  useEffect(() => {
+    filterHandler(filter);
+  }, [filter]);
+
   useEffect(() => {
     getMatches();
   }, []);
@@ -39,19 +69,31 @@ const MatchesPage: React.FC<Props> = ({
   return (
     <PageContainer>
       <div className="mb-8">
-        <h1 className="text-3xl border-b pb-1 border-gray-400 text-gray-400 font-bold mb-5">
-          Partidas Configuradas
-        </h1>
-
         <div className="mb-8">
           <GameForm addHandler={createMatch} getMatches={getMatches} />
         </div>
 
+        <div className="border-b pb-1 border-gray-400 flex gap-5 justify-between items-end">
+          <h1 className="text-xl md:text-3xl text-gray-400 font-bold">
+            Partidas Configuradas
+          </h1>
+
+          <div className="w-48">
+            <Select
+              label="Filtrar por"
+              options={sortOptions}
+              defaultOption={sortOptions[0]}
+              onChange={(e) =>
+                setFilter(e.target.value.toLocaleLowerCase() as ISortOptions)
+              }
+            />
+          </div>
+        </div>
+
         {!isLoading && matches.length > 0 ? (
           <MatchList
-            matches={matches}
-            getMatches={getMatches}
-            toggleStatusMatchFactory={toggleStatusMatchFactory}
+            matches={filter !== "todos" ? filteredMatches : matches}
+            toggleStatusHandler={toggleStatusHandler}
           />
         ) : (
           <div className="mt-24 text-center">
