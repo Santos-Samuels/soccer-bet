3ed0b471c85f159acde931d3cbb3747a5ef8b0e5
Santos-Samuels <saamsmith15@gmail.com>
2023-02-12 15:57:44 -0300

    🚚 moving domain match usecases

--- src/domain/usecases/match/createMatch.ts
+++ src/domain/usecases/match/createMatch.ts
@@ -1,5 +1,5 @@
 import { IInputMatch } from "@data/dto/input/match";
-import { IMatch } from "../model/match";
+import { IMatch } from "../../model/match";
 
 export default interface ICreateMatch {
   execute(input: IInputMatch): Promise<IMatch>
