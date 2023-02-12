e33408a96cc5e0d4d373ad9018b09229d32e8f85
Santos-Samuels <saamsmith15@gmail.com>
2023-02-11 23:19:34 -0300

    ✨ adding list match page factory

---
+++ src/main/factories/pages/matchesPageFactory.tsx
@@ -0,0 +1,14 @@
+import { MatchesPage } from "../../../presentation/pages";
+import { createMatchFactory } from "../usecases/createMatchFactory";
+import { listMatchesFactory } from "../usecases/listMatchesFactory";
+import { toggleStatusMatchFactory } from "../usecases/toggleStatusMatchFactory";
+
+export const MatchesPageFactory: React.FC = () => {
+  return (
+    <MatchesPage
+      listMatches={listMatchesFactory()}
+      createMatch={createMatchFactory()}
+      toggleStatusMatchFactory={toggleStatusMatchFactory()}
+    />
+  );
+};
