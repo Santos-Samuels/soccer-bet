7ef6227e09ead549e47e43431a987f1ba3004606
Santos-Samuels <saamsmith15@gmail.com>
2023-02-13 08:15:53 -0300

    ♻️ some matches page ajustments

--- src/presentation/pages/MatchesPage.tsx
+++ src/presentation/pages/MatchesPage.tsx
@@ -68,12 +68,11 @@ const MatchesPage: React.FC<Props> = ({
 
   return (
     <PageContainer>
-      <div className="mb-8">
         <div className="mb-8">
           <GameForm addHandler={createMatch} getMatches={getMatches} />
         </div>
 
-        <div className="border-b pb-1 border-gray-400 flex gap-5 justify-between items-end">
+        <div className="border-b pb-1 border-gray-400 flex gap-5 justify-between items-end mb-8">
           <h1 className="text-xl md:text-3xl text-gray-400 font-bold">
             Partidas Configuradas
           </h1>
@@ -100,7 +99,6 @@ const MatchesPage: React.FC<Props> = ({
             <BeatLoader color="#eab308" size={30} />
           </div>
         )}
-      </div>
     </PageContainer>
   );
 };
