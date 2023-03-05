015883ce7a07c7f9ae53083db9bddc3cc413ef4c
Santos-Samuels <saamsmith15@gmail.com>
2023-03-04 22:11:48 -0300

    ✨ implementing 404 and 401 pages

--- src/presentation/pages/NotFoundPage.tsx
+++ src/presentation/pages/NotFoundPage.tsx
@@ -1,5 +1,23 @@
+import { useNavigate } from "react-router-dom";
+
 const NotFoundPage: React.FC = () => {
-  return <h1>NotFoundPage</h1>;
+  const navigate = useNavigate()
+
+  return (
+    <div className="flex flex-col items-center justify-center h-screen">
+      <h1 className="text-4xl font-bold">404</h1>
+      <h2 className="text-xl">Página não encontrada</h2>
+
+      <div className="mt-8">
+        <button
+          onClick={() => navigate(-1)}
+          className="bg-yellow-600 text-white px-4 py-2 rounded-md hover:bg-yellow-500"
+        >
+          Voltar
+        </button>
+        </div>
+    </div>
+  );
 };
 
 export default NotFoundPage;
--- src/presentation/pages/UnauthorizedPage.tsx
+++ src/presentation/pages/UnauthorizedPage.tsx
@@ -1,5 +1,10 @@
 const UnauthorizedPage: React.FC = () => {
-  return <h1>UnauthorizedPage</h1>;
+  return (
+    <div className="flex flex-col items-center justify-center h-screen">
+      <h1 className="text-4xl font-bold">401</h1>
+      <h2 className="text-xl">Você não tem permissão para acessar essa página</h2>
+    </div>
+  );
 };
 
 export default UnauthorizedPage;
